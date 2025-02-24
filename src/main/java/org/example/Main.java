37818347e08d9fe64d592395382f0bc4a3773424
tonymekhail <amekha1465@inst.hcpss.org>
2025-02-24 18:54:40 +0000

    02/24/25 1:54

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,31 @@
-package org.example; //DO NOT DELETE
+package org.example;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       double sgallons;
+       double gallons;
+       double quarts;
+       double pints;
+       double cups;
+       double tablespoons;
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number of gallons: ");
+       sgallons = input.nextDouble();
+       gallons = sgallons * 1;
+       quarts = gallons * 4;
+       pints = quarts * 2;
+       cups = pints * 2;
+       tablespoons = cups * 16;
+       System.out.println("In " + gallons + " gallons there are:");
+       System.out.println(quarts + " quarts");
+       System.out.println(pints + " pints");
+       System.out.println(cups + " cups");
+       System.out.println(tablespoons + " tablespoons");
+
+
+
+
+
+    }
+}
